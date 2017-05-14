bb95b931084d2f879ad594773f0db4bda68780bd
Jack Le <e0146768@u.nus.edu>
2017-05-14 21:10:23 +0800

    Submit project base

---
+++ demo/simple/layout.js
@@ -0,0 +1,104 @@
+(function() {
+  "use strict";
+
+  var ENTER_KEY_CODE = 13;
+  var ACCESS_TOKEN = "3501b98607d34968861fc82e76ae4553";
+  var queryInput, resultDiv, accessTokenInput;
+
+  window.onload = init;
+  window.onpageshow = onShow;
+
+  function onShow() {
+    console.log("on Show");
+    document.getElementById("placeholder").style.display = "none";
+    document.getElementById("main-wrapper").style.display = "block";
+    window.init(ACCESS_TOKEN);
+  }
+
+  function init() {
+    queryInput = document.getElementById("q");
+    resultDiv = document.getElementById("result");
+    
+    var sendMessageButton = document.getElementById("send_message");
+    sendMessageButton.addEventListener("click", queryInputKeyDown);
+  }
+
+  function setAccessToken() {
+    document.getElementById("placeholder").style.display = "none";
+    document.getElementById("main-wrapper").style.display = "block";
+    window.init(accessTokenInput.value);
+  }
+
+  function queryInputKeyDown() {
+    console.log("queryInputKeyDown");
+
+    var value = queryInput.value;
+    queryInput.value = "";
+
+    createQueryNode(value);
+    var responseNode = createResponseNode();
+
+    sendText(value)
+      .then(function(response) {
+        var result;
+        try {
+          result = response.result.fulfillment.speech
+        } catch(error) {
+          result = "";
+        }
+        setResponseJSON(response);
+        setResponseOnNode(result, responseNode);
+      })
+      .catch(function(err) {
+        setResponseJSON(err);
+        setResponseOnNode("Something goes wrong", responseNode);
+      });
+  }
+
+  function createQueryNode(query) {
+    var node = document.createElement('div');
+    node.className = "clearfix left-align left card-panel green accent-1";
+    node.innerHTML = query;
+    resultDiv.appendChild(node);
+  }
+
+  function createResponseNode() {
+    var node = document.createElement('div');
+    node.className = "clearfix right-align right card-panel blue-text text-darken-2 hoverable";
+    node.innerHTML = "...";
+    resultDiv.appendChild(node);
+    return node;
+  }
+
+  function setResponseOnNode(response, node) {
+    node.innerHTML = response ? response : "[empty response]";
+    node.setAttribute('data-actual-response', response);
+    var speaking = false;
+    
+    function speakNode() {
+      if (!response || speaking) {
+        return;
+      }
+      speaking = true;
+      tts(response)
+        .then(function () {speaking = false})
+        .catch(function (err) {
+          speaking = false;
+          Materialize.toast(err, 2000, 'red lighten-1');
+        });
+    }
+
+    node.addEventListener("click", speakNode);
+    speakNode();
+  }
+
+  function setResponseJSON(response) {
+    var node = document.getElementById("jsonResponse");
+    node.innerHTML = JSON.stringify(response, null, 2);
+  }
+
+  function sendRequest() {
+
+  }
+
+})();
